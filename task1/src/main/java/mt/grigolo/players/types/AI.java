3a967be2f833058ce21f79e53a58a86a14385335
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-01-08 22:06:50 +0100

    Added player types

---
+++ task1/src/main/java/mt/grigolo/players/types/AI.java
@@ -0,0 +1,16 @@
+package mt.grigolo.players.types;
+
+import mt.grigolo.players.Player;
+
+public class AI extends Player {
+
+    public AI(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void playerInput() {
+        //TODO: Random choices
+    }
+
+}
---
+++ task1/src/main/java/mt/grigolo/players/types/Human.java
@@ -0,0 +1,17 @@
+package mt.grigolo.players.types;
+
+import mt.grigolo.players.Player;
+
+public class Human extends Player {
+
+
+    public Human(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void playerInput() {
+        // TODO Auto-generated method stub
+
+    }
+}
