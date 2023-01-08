9875bf39060a3e11413c9a020fadbc4c92299105
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-01-08 23:27:01 +0100

    Added LevelableObject and tests

--- task1/src/main/java/mt/grigolo/utils/LevelableObject.java
+++ task1/src/main/java/mt/grigolo/utils/LevelableObject.java
@@ -1,2 +1,55 @@
-package mt.grigolo.utils;public class LevelableObject {
+package mt.grigolo.utils;
+
+import mt.grigolo.exceptions.InsufficientResourceException;
+import mt.grigolo.exceptions.MaxLevelException;
+import mt.grigolo.resources.Resource;
+
+public abstract class LevelableObject {
+
+
+    private int level = 1;
+    private final int maxLevel;
+    private int levelUpCost;
+    private int costIncrease;
+
+    private Resource levelUpResource;
+
+    private final static double costIncreaseFactor = 1.2;
+
+    public LevelableObject(int maxLevel, int levelUpCost, int costIncrease, Resource levelUpResource) {
+        this.maxLevel = maxLevel;
+        this.levelUpCost = levelUpCost;
+        this.costIncrease = costIncrease;
+        this.levelUpResource = levelUpResource;
+    }
+
+    public void levelUp() throws InsufficientResourceException, MaxLevelException {
+        if (level > maxLevel) {
+            throw new MaxLevelException();
+        } else if (levelUpResource.getAmount() < levelUpCost) {
+            throw new InsufficientResourceException();
+        } else {
+            level++;
+            this.levelUpResource.decrement(levelUpCost);
+            levelUpCost += costIncrease;
+            costIncrease *= costIncreaseFactor;
+            levelUpLogic();
+        }
+    }
+
+    public int getLevelUpCost() {
+        return levelUpCost;
+    }
+
+    public abstract void levelUpLogic();
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevelUpResource(Resource levelUpResource) {
+        this.levelUpResource = levelUpResource;
+    }
+
+
 }
--- task1/src/test/java/mt/grigolo/utils/LevelableObjectTest.java
+++ task1/src/test/java/mt/grigolo/utils/LevelableObjectTest.java
@@ -1,2 +1,51 @@
-package mt.grigolo.utils;public class LevelableObjectTest {
+package mt.grigolo.utils;
+
+import mt.grigolo.exceptions.InsufficientResourceException;
+import mt.grigolo.exceptions.MaxLevelException;
+import mt.grigolo.resources.Resource;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LevelableObjectTest {
+    LevelableObject levelableObject = new LevelableObject(10, 10, 10, new Resource(1000, 1000) {
+        @Override
+        public String getName(boolean plural) {
+            return "Test Currency";
+        }
+    }) {
+        @Override
+        public void levelUpLogic() {
+            // do nothing
+        }
+    };
+
+
+    @Test
+    public void levelUp() throws MaxLevelException, InsufficientResourceException {
+        for (int i = 0; i < 2; i++) {
+            levelableObject.levelUp();
+        }
+        assertEquals(3, levelableObject.getLevel());
+        assertEquals(32, levelableObject.getLevelUpCost());
+    }
+
+    @Test(expected = MaxLevelException.class)
+    public void levelUpMaxLevel() throws MaxLevelException, InsufficientResourceException {
+        for (int i = 0; i < 10; i++) {
+            levelableObject.levelUp();
+        }
+        levelableObject.levelUp();
+    }
+
+    @Test(expected = InsufficientResourceException.class)
+    public void levelUpInsufficientResource() throws MaxLevelException, InsufficientResourceException {
+        levelableObject.setLevelUpResource(new Resource(0, 0) {
+            @Override
+            public String getName(boolean plural) {
+                return "Test Currency";
+            }
+        });
+        levelableObject.levelUp();
+    }
 }
