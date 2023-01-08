4ab2de5f2670c56012be0d7c4433e482202a6cf2
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-01-08 23:36:37 +0100

    Added global variables for gameplay finetuning

---
+++ task1/src/main/java/mt/grigolo/Globals.java
@@ -0,0 +1,8 @@
+package mt.grigolo;
+
+public class Globals {
+
+    public static final float TroopGeneratorDiscountIncrease = 0.03F;
+    public static final int initialVillageResourceAmount = 100;
+
+}
