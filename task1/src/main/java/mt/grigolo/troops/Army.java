8fc85b07a509fc3950e9b8031e0111faccd8f84b
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-01-08 23:37:39 +0100

    Army rehaul with tests

--- task1/src/main/java/mt/grigolo/troops/Army.java
+++ task1/src/main/java/mt/grigolo/troops/Army.java
@@ -6,54 +6,162 @@ import mt.grigolo.utils.Position;
 
 import java.util.ArrayList;
 
-public class Army {
+public class Army extends ArrayList<Troop> {
 
-    private final ArrayList<Troop> troops = new ArrayList<>();
 
-    public int maxTroops;
-    private boolean stationed;
+    private int maxTroops;
+
+    private int ticksUntilArrival;
+
+    public boolean isInRange(Village v) {
+        return destination == v && ticksUntilArrival == 0;
+    }
 
     public void addTroop(Troop troop) throws ArmyFullException {
-        if (troops.size() < maxTroops) {
-            troops.add(troop);
-        } else
-            throw new ArmyFullException();
+        if (this.size() < getMaxTroops()) {
+            add(troop);
+        } else throw new ArmyFullException();
     }
 
-    public ArrayList<Troop> getTroops() {
-        return troops;
-    }
 
     private final Village sourceVillage;
-    private Village targetVillage;
+
+    private Village destination;
 
     Position currentPos;
 
+    public int getTicksUntilArrival() {
+        return ticksUntilArrival;
+    }
+
+    public void setTicksUntilArrival(int ticksUntilArrival) {
+        this.ticksUntilArrival = ticksUntilArrival;
+    }
+
+    public void march() {
+        if (destination != null && ticksUntilArrival > 0) ticksUntilArrival--;
+    }
+
     public Army(Village sourceVillage) {
         this.sourceVillage = sourceVillage;
+        ticksUntilArrival = 0;
         currentPos = sourceVillage.pos;
-        maxTroops = 2;
+        setMaxTroops(2);
     }
 
     public void emptyInventory() {
-        if(currentPos.equals(sourceVillage.pos)) {
-            for (Troop troop : troops) {
-                troop.getInventory().transferTo(sourceVillage.getGemStorage());
-                troop.getInventory().transferTo(sourceVillage.getGoldStorage());
-                troop.getInventory().transferTo(sourceVillage.getElixirStorage());
-            }
-            stationed = true;
+        for (Troop troop : this) {
+            sourceVillage.takeFromTroop(troop, troop.getInventory().getAmount());
         }
+        destination = null;
+    }
+
+    public Village getDestination() {
+        return destination;
+    }
+
+    public Village getSourceVillage() {
+        return sourceVillage;
+    }
+
+    public void setDestination(Village destination) {
+        this.destination = destination;
+        ticksUntilArrival = (int) Math.ceil((double) (Position.distance(sourceVillage.pos, destination.pos) - getArmyMinSpeed()) / getArmyMinSpeed());
+    }
+
+    public void initiateAttack(Village enemyVillage) {
+        enemyVillage.getEnemyArmies().add(this);
+        setDestination(enemyVillage);
+    }
+
+    public int attackValue() {
+        int totalDamage = 0;
+        for (Troop troop : this) {
+            totalDamage += troop.getAttack();
+        }
+        return totalDamage;
+    }
+
+
+    public boolean attack(Village defendingVillage) {
+        for (Troop troop : this) {
+            if (!defendingVillage.getArmy().isEmpty() && defendingVillage.getArmy().isInHomeVillage()) {
+
+                // Each troop attacks a random enemy troop
+                troop.fight(defendingVillage.getArmy().get((int) (Math.random() * defendingVillage.getArmy().size())));
+
+                // If any troop is dead, remove it from the army
+                this.cleanCorpses();
+                defendingVillage.getArmy().cleanCorpses();
+
+                // stay in the village until there are no more troops
+                return false;
+
+            } else {
+
+                // If the defending village has no army, or it is away, attack the village directly
+                defendingVillage.damage(troop.getAttack());
+
+                // take as much resource as possible from the village
+                defendingVillage.giveToTroop(troop, troop.getInventory().getMaxAmount());
+
+                // If the village has been hit, send the troops back to their home
+                return true;
+            }
+        }
+
+        if (!defendingVillage.getArmy().isEmpty() && defendingVillage.getArmy().isInHomeVillage()) {
+            for (Troop troop : this) {
+                troop.fight(defendingVillage.getArmy().get((int) (Math.random() * defendingVillage.getArmy().size())));
+                defendingVillage.getArmy().cleanCorpses();
+            }
+            this.cleanCorpses();
+        } else {
+            defendingVillage.damage(attackValue());
+        }
+        return false;
+    }
+
+    public void cleanCorpses() {
+        removeIf(Troop::isDead);
     }
 
     @Override
     public String toString() {
-        StringBuilder s = new StringBuilder("\t Capacity: " + this.troops.size() + "/" + this.maxTroops + "\n");
-        for (Troop troop : troops) {
+        StringBuilder s = new StringBuilder("\t Capacity: " + size() + "/" + this.getMaxTroops() + "\n");
+        for (Troop troop : this) {
             s.append("\t ").append(troop.toString()).append("\n");
         }
         return s.toString();
     }
 
+    @Override
+    public boolean add(Troop troop) {
+        if (this.size() < getMaxTroops()) {
+            return super.add(troop);
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isInHomeVillage() {
+        return destination == null && isInRange(sourceVillage);
+    }
+
+    public int getMaxTroops() {
+        return maxTroops;
+    }
+
+    public void setMaxTroops(int maxTroops) {
+        this.maxTroops = maxTroops;
+    }
+
+    private int getArmyMinSpeed() {
+        int minSpeed = Integer.MAX_VALUE;
+        for (Troop troop : this) {
+            if (troop.getMarchingSpeed() < minSpeed) minSpeed = troop.getMarchingSpeed();
+        }
+        return minSpeed;
+    }
 
 }
--- task1/src/test/java/mt/grigolo/troops/ArmyTest.java
+++ task1/src/test/java/mt/grigolo/troops/ArmyTest.java
@@ -1,5 +1,7 @@
 package mt.grigolo.troops;
 
+import mt.grigolo.Globals;
+import mt.grigolo.exceptions.ArmyFullException;
 import mt.grigolo.exceptions.InsufficientResourceException;
 import mt.grigolo.exceptions.MaxLevelException;
 import mt.grigolo.players.Village;
@@ -10,8 +12,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class ArmyTest {
     Village v;
@@ -26,37 +27,70 @@ public class ArmyTest {
         Barbarian barbarian = new Barbarian();
         Goblin goblin = new Goblin();
 
-        archer.getInventory().setAmount(50);
-        barbarian.getInventory().setAmount(50);
-        goblin.getInventory().setAmount(50);
+        archer.getInventory().setAmount(archer.getInventory().getMaxAmount());
+        barbarian.getInventory().setAmount(barbarian.getInventory().getMaxAmount());
+        goblin.getInventory().setAmount(goblin.getInventory().getMaxAmount());
 
-        v.getArmy().getTroops().add(archer);
-        v.getArmy().getTroops().add(barbarian);
-        v.getArmy().getTroops().add(goblin);
+        v.getArmy().add(archer);
+        v.getArmy().add(barbarian);
+        v.getArmy().add(goblin);
     }
 
     @Test
     public void emptyInventory() throws MaxLevelException, InsufficientResourceException {
 
-        v.getGemStorage().increment(1000);
-        v.levelSystem.levelUp();
-
-        assertEquals(600, v.getGemStorage().getAmount());
+        assertEquals(Globals.initialVillageResourceAmount, v.getGemStorage().getAmount());
 
 
         v.getArmy().emptyInventory();
 
-        for (Troop t : v.getArmy().getTroops()) {
+        for (Troop t : v.getArmy()) {
             Assert.assertEquals(0, t.getInventory().getAmount());
         }
 
-        assertEquals(650, v.getGemStorage().getAmount());
+        assertEquals(110, v.getGemStorage().getAmount());
         assertEquals(150, v.getGoldStorage().getAmount());
-        assertEquals(150, v.getElixirStorage().getAmount());
+        assertEquals(100, v.getElixirStorage().getAmount());
     }
 
     @Test
     public void testArmyFull() {
 
+    }
+
+    @Test
+    public void marchTest() throws ArmyFullException {
+        Village v1 = new Village(0, 0);
+        Village v2 = new Village(10, 1);
+
+        v1.getArmy().addTroop(new Archer());
+        v1.getArmy().setDestination(v2);
+
+        assertEquals(1, v1.getArmy().getTicksUntilArrival());
+
+        v1.getArmy().march();
+
+        assertEquals(0, v1.getArmy().getTicksUntilArrival());
+
+        assertTrue(v1.getArmy().isInRange(v2));
+    }
+
+
+    @Test
+    public void initiateAttack() throws ArmyFullException {
+        Village v1 = new Village(0, 0);
+        Village v2 = new Village(10, 1);
+        v1.getArmy().addTroop(new Archer());
+        v1.getArmy().initiateAttack(v2);
+
+        assertFalse(v1.getArmy().isInRange(v2));
+        assertEquals(1, v1.getArmy().getTicksUntilArrival());
+
+        v1.getArmy().march();
+
+        assertTrue(v1.getArmy().isInRange(v2));
+        assertEquals(0, v1.getArmy().getTicksUntilArrival());
+        assertEquals(1, v2.getEnemyArmies().size());
+        assertEquals(v1.getArmy(), v2.getEnemyArmies().get(0));
     }
 }
