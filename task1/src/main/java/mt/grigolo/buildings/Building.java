4add12d55d14900a5d15c7655e91fdfcc0b61cca
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-01-08 23:32:48 +0100

    Building now has new leveling inheritance

--- task1/src/main/java/mt/grigolo/buildings/Building.java
+++ task1/src/main/java/mt/grigolo/buildings/Building.java
@@ -1,26 +1,19 @@
 package mt.grigolo.buildings;
 
-import mt.grigolo.utils.LevelSystem;
+import mt.grigolo.exceptions.InsufficientResourceException;
+import mt.grigolo.resources.Resource;
 import mt.grigolo.players.Player;
+import mt.grigolo.utils.LevelableObject;
 
-public abstract class Building {
+public abstract class Building extends LevelableObject {
 
-    LevelSystem levelSystem;
 
-    public Building(){
-
-    }
-
-    public LevelSystem getLevelSystem() {
-        return levelSystem;
-    }
-
-    public void setLevelSystem(LevelSystem levelSystem) {
-        this.levelSystem = levelSystem;
+    public Building(Resource levelUpResource, int maxLevel, int levelUpCost, int costIncrease) {
+        super(maxLevel, levelUpCost, costIncrease, levelUpResource);
     }
 
     public abstract void doTick();
 
-    protected abstract void interact(Player player);
+    protected abstract void interact() throws InsufficientResourceException;
 
 }
