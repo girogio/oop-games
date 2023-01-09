e9e59ce64614a80129723f64f2bca97bd8257c5a
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-01-09 01:08:09 +0100

    Added map Class

---
+++ task1/src/main/java/mt/grigolo/game/Map.java
@@ -0,0 +1,76 @@
+package mt.grigolo.game;
+
+import mt.grigolo.players.Player;
+import mt.grigolo.players.types.AI;
+import mt.grigolo.players.types.Human;
+
+import java.util.ArrayList;
+
+public class Map {
+
+    private final int width;
+    private final int height;
+
+    private final ArrayList<Player> players = new ArrayList<>();
+
+    public Map(int width, int height, int playerCount, int aiCount) {
+        this.width = width;
+        this.height = height;
+
+        int x, y;
+
+        for (int i = 0; i < playerCount; i++) {
+            do {
+                x = (int) (Math.random() * width);
+                y = (int) (Math.random() * height);
+            } while (isOccupied(x, y));
+            players.add(new Human(x, y, i + 1));
+        }
+
+        for (int i = 0; i < aiCount; i++) {
+            do {
+                x = (int) (Math.random() * width);
+                y = (int) (Math.random() * height);
+            } while (isOccupied(x, y));
+            players.add(new AI(x, y, playerCount + i + 1));
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                boolean found = false;
+                for (Player player : players) {
+                    if (player.getVillage().pos.getX() == j && player.getVillage().pos.getY() == i) {
+                        if (player instanceof Human) {
+                            s.append(" H ");
+                        } else if (player instanceof AI) {
+                            s.append(" A ");
+                        }
+                        found = true;
+                    }
+                }
+                if (!found)
+                    s.append("[ ]");
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    protected boolean isOccupied(int x, int y) {
+        for (Player player : players) {
+            if (player.getVillage().pos.getX() == x && player.getVillage().pos.getY() == y) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
