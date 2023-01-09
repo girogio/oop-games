f76e73b7075437a547a802292ddec445d0a33f41
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-01-09 01:08:34 +0100

    Added human playerinput buily menu entry

--- task1/src/main/java/mt/grigolo/players/types/Human.java
+++ task1/src/main/java/mt/grigolo/players/types/Human.java
@@ -13,6 +13,40 @@ public class Human extends Player {
 
     @Override
     public void playerInput() {
-        
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(getVillage());
+
+        System.out.println("1. Build");
+        System.out.println("2. Upgrade");
+        System.out.println("3. Train");
+        System.out.println("4. Upgrade");
+        System.out.println("5. Surrender");
+        System.out.println("6. Pass");
+        System.out.println("> ");
+
+
+        int choice = 0;
+
+        do {
+            choice = scanner.nextInt();
+        } while (choice < 1 || choice > 6);
+
+        switch (choice) {
+            case 1 -> {
+                System.out.println("1. Resource Generator");
+                System.out.println("2. Troop Generator");
+                int building = scanner.nextInt();
+                System.out.println("1. Gem Generator");
+                System.out.println("2. Gold Generator");
+                System.out.println("3. Elixir Generator");
+                int type = scanner.nextInt();
+                build(building, type);
+            }
+        }
+
+        scanner.close();
+
     }
 }
