bd248fcc33c2c7fadf1ced3ed8406bb83df4b7f3
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-01-09 18:31:06 +0100

    Added ResourceGenerator global settings

--- task1/src/main/java/mt/grigolo/Globals.java
+++ task1/src/main/java/mt/grigolo/Globals.java
@@ -3,6 +3,15 @@ package mt.grigolo;
 public class Globals {
 
     public static final float TroopGeneratorDiscountIncrease = 0.03F;
-    public static final int initialVillageResourceAmount = 100;
+    public static final int initialVillageResourceAmount = 1000;
+
+    // Resource Generator
+    public static final int resGenMaxLevel = 10;
+    public static final int resGenLevelUpCost = 30;
+    public static final int resGenCostIncrease = 50;
+    public static final int resGenBuildCost = 50;
+    public static final int resGenDefaultGenPerTick = 10;
+    public static final int resGenDefaultGenIncrease = 10;
+    public static final double resGenIncFactor = 1.2;
 
 }
