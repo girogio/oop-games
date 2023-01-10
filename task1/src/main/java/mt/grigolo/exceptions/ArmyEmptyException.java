2a701c453297a432bef85bfcd30add1f2f4990be
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-01-11 00:36:24 +0100

    Updated ArmyEmptyException message

--- task1/src/main/java/mt/grigolo/exceptions/ArmyEmptyException.java
+++ task1/src/main/java/mt/grigolo/exceptions/ArmyEmptyException.java
@@ -2,6 +2,6 @@ package mt.grigolo.exceptions;
 
 public class ArmyEmptyException extends Exception{
     public ArmyEmptyException() {
-        super("Army is empty.");
+        super("Army is empty. Train some troops first.");
     }
 }
