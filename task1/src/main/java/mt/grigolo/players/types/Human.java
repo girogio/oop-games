e8aafd56f8e8542cd0d8ea542f744b4d4d131771
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-01-10 17:37:00 +0100

    Added user menu

--- task1/src/main/java/mt/grigolo/players/types/Human.java
+++ task1/src/main/java/mt/grigolo/players/types/Human.java
@@ -1,180 +1,212 @@
 package mt.grigolo.players.types;
 
 import mt.grigolo.buildings.Building;
-import mt.grigolo.buildings.types.ResourceGenerator;
-import mt.grigolo.buildings.types.TroopGenerator;
-import mt.grigolo.exceptions.ArmyAwayException;
-import mt.grigolo.exceptions.ArmyFullException;
-import mt.grigolo.exceptions.InsufficientResourceException;
+import mt.grigolo.buildings.types.*;
+import mt.grigolo.exceptions.*;
+import mt.grigolo.game.Map;
 import mt.grigolo.players.Player;
 import mt.grigolo.resources.types.Elixir;
 import mt.grigolo.resources.types.Gem;
 import mt.grigolo.resources.types.Gold;
-import mt.grigolo.troops.types.Archer;
-import mt.grigolo.troops.types.Barbarian;
-import mt.grigolo.troops.types.Goblin;
+import mt.grigolo.utils.Input;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Human extends Player {
 
+    private Map map;
 
     public Human(int x, int y, int id) {
         super(x, y, id);
     }
 
+    public void setMap(Map map) {
+        this.map = map;
+    }
+
     @Override
     public void playerInput() {
 
-        Scanner scanner = new Scanner(System.in);
         int choice;
 
-        do {
-            System.out.println(getVillage());
+        Input.clearScreen();
 
-            System.out.println("1. Build");
-            System.out.println("2. Upgrade");
-            System.out.println("3. Train");
-            System.out.println("4. Attack");
-            System.out.println("5. Surrender");
-            System.out.println("6. Pass");
-
-            do {
-                System.out.print("> ");
-                choice = scanner.nextInt();
+        System.out.println("Round " + map.round + ", player " + getId() + "'s turn.");
+        System.out.println(map);
 
 
-                System.out.print("\033[H\033[2J");
+        System.out.println(getVillage());
 
-                switch (choice) {
-                    case 1 -> {
-                        int building, type = 0;
-                        boolean purchaseSuccesful = false;
+        System.out.println("1. Build");
+        System.out.println("2. Upgrade");
+        System.out.println("3. Train");
+        System.out.println("4. Attack");
+        System.out.println("5. Surrender");
+        System.out.println("6. Pass");
+        System.out.print("> ");
 
-                        do { // until purchase successful
+        choice = Input.getInt(1, 6);
 
+        if (choice == 1) {
 
-                            do { // until valid building type
+            int building, type;
 
-                                System.out.println("1. Resource Generator");
-                                System.out.println("2. Troop Generator");
-                                System.out.println("3. Back");
-                                System.out.print("> ");
-                                building = scanner.nextInt();
+            Input.clearScreen();
+            System.out.println("1. Resource Generator");
+            System.out.println("2. Troop Generator");
+            System.out.println("3. Back");
+            System.out.print("> ");
+            building = Input.getInt(1, 3);
 
-                            } while (building < 1 || building > 3);
+            Input.clearScreen();
 
-                            if (building == 3) {
-                                break;
-                            }
+            if (building == 1) {
 
-                            switch (building) {
+                ResourceGenerator goldGen = new ResourceGenerator(new Gold(), new Elixir());
+                ResourceGenerator gemGen = new ResourceGenerator(new Gem(), new Gold());
+                ResourceGenerator elixirGen = new ResourceGenerator(new Elixir(), new Gem());
 
-                                // if resource generator
-                                case 1 -> {
+                System.out.println("1. " + goldGen.getBuildCostString() + " – " + goldGen);
+                System.out.println("2. " + gemGen.getBuildCostString() + " – " + gemGen);
+                System.out.println("3. " + elixirGen.getBuildCostString() + " – " + elixirGen);
 
-                                    do { // until valid sub building type
-                                        System.out.print("\033[H\033[2J");
-                                        ResourceGenerator goldGen = new ResourceGenerator(new Gold(), new Elixir());
-                                        ResourceGenerator gemGen = new ResourceGenerator(new Gem(), new Gold());
-                                        ResourceGenerator elixirGen = new ResourceGenerator(new Elixir(), new Gem());
+            } else if (building == 2) {
 
-                                        System.out.println("1. " + goldGen.getBuildCostString() + " – " + goldGen);
-                                        System.out.println("2. " + gemGen.getBuildCostString() + " – " + gemGen);
-                                        System.out.println("3. " + elixirGen.getBuildCostString() + " – " + elixirGen);
+                TroopGenerator placeHolderGen = new ArcherGenerator(getVillage());
+                System.out.println("1. " + placeHolderGen.getBuildCostString() + " – " + placeHolderGen);
 
-                                        System.out.println("4. Back");
-                                        System.out.print("> ");
-                                        type = scanner.nextInt();
+                placeHolderGen = new GoblinGenerator(getVillage());
+                System.out.println("2. " + placeHolderGen.getBuildCostString() + " – " + placeHolderGen);
 
-                                    } while (type < 1 || type > 4);
-                                }
+                placeHolderGen = new BarbarianGenerator(getVillage());
+                System.out.println("3. " + placeHolderGen.getBuildCostString() + " – " + placeHolderGen);
 
-                                // if troop generator1
-                                case 2 -> {
-                                    do { //until valid sub building type
-                                        System.out.print("\033[H\033[2J");
-                                        TroopGenerator placeHolderGen = new TroopGenerator(new Archer(), getVillage().getArmy(), getVillage().getGemStorage());
-                                        System.out.println("1. " + placeHolderGen.getBuildCostString() + " – " + placeHolderGen);
+            } else if (building == 3) {
+                playerInput();
+            }
 
-                                        placeHolderGen = new TroopGenerator(new Goblin(), getVillage().getArmy(), getVillage().getGoldStorage());
-                                        System.out.println("2. " + placeHolderGen.getBuildCostString() + " – " + placeHolderGen);
+            System.out.println("4. Back");
+            System.out.print("> ");
 
-                                        placeHolderGen = new TroopGenerator(new Barbarian(), getVillage().getArmy(), getVillage().getElixirStorage());
-                                        System.out.println("3. " + placeHolderGen.getBuildCostString() + " – " + placeHolderGen);
+            type = Input.getInt(1, 4);
 
-                                        System.out.println("4. Back");
-                                        System.out.print("> ");
-                                        type = scanner.nextInt();
-                                    } while (type < 1 || type > 4);
-                                }
-                            }
-
-                            try {
-                                purchaseSuccesful = build(building, type);
-                            } catch (InsufficientResourceException e) {
-                                System.out.println("\n\n" + e.getMessage() + "\n\n");
-                            }
-
-                        } while (!purchaseSuccesful);
-                    }
-
-                    case 2 -> {
-                        int buildingToLevelUp;
-                        do {
-                            System.out.println("Choose building to upgrade\n ");
-                            for (int i = 0; i < getVillage().getBuildings().size(); i++) {
-                                System.out.println(i + 1 + ". Cost: " + getVillage().getBuildings().get(i).getLevelUpString() + " – "
-                                        + getVillage().getBuildings().get(i) + "\n");
-                            }
-                            System.out.println(getVillage().getBuildings().size() + 1 + ". Back");
-                            System.out.print("> ");
-                            buildingToLevelUp = scanner.nextInt();
-                        } while (buildingToLevelUp > getVillage().getBuildings().size() + 1 || buildingToLevelUp < 1);
-
-                        if (buildingToLevelUp != getVillage().getBuildings().size() + 1) {
-                            try {
-                                getVillage().levelUpBuilding(buildingToLevelUp - 1);
-                            } catch (Exception e) {
-                                System.out.println(e.getMessage());
-                                scanner.nextLine();
-                            }
-                        } else {
-                            System.out.println("\n\n\n\n\n\n");
-                        }
-                    }
-
-                    case 3 -> {
-                        System.out.println("Choose troop to train\n ");
-
-                        for (int i = 0; i < getVillage().getBuildings().stream().filter(building -> building instanceof TroopGenerator).count(); i++) {
-                            System.out.println(i + 1 + ". Cost: " + getVillage().getBuildings().stream().filter(building -> building instanceof TroopGenerator).toArray()[i]);
-                        }
-
-                        System.out.println(getVillage().getBuildings().stream().filter(building -> building instanceof TroopGenerator).count() + 1 + ". Back");
-
-                        System.out.print("> ");
-
-                        ArrayList<Integer> troopGenerators = new ArrayList<>();
-
-                        for (Building building : getVillage().getBuildings()) {
-                            if (building instanceof TroopGenerator) {
-                                troopGenerators.add(getVillage().getBuildings().indexOf(building));
-                            }
-                        }
-
-                        int troopToTrain = scanner.nextInt();
-
-                        try {
-                            getVillage().getBuildings().get(troopGenerators.get(troopToTrain - 1)).interact();
-                        } catch (InsufficientResourceException | ArmyFullException | ArmyAwayException e) {
-                            System.out.println("\n\n" + e.getMessage() + "\n\n");
-                        }
-                    }
-
+            if (type == 4) {
+                playerInput();
+            } else {
+                try {
+                    build(building, type);
+                } catch (InsufficientResourceException e) {
+                    System.out.println("\n\n" + e.getMessage() + "\n\n");
                 }
-            } while (choice < 1 || choice > 6);
-        } while (choice != 6);
+            }
+
+        } else if (choice == 2) {
+
+            int buildingToLevelUp;
+            Input.clearScreen();
+            System.out.println("Choose building to upgrade\n ");
+            System.out.println("1. " + getVillage().getLevelUpString() + " – " + getVillage());
+
+            for (int i = 0; i < getVillage().getBuildings().size(); i++) {
+                System.out.println(i + 2 + ". Cost: " + getVillage().getBuildings().get(i).getLevelUpString() + " – "
+                        + getVillage().getBuildings().get(i) + "\n");
+            }
+
+            System.out.println(getVillage().getBuildings().size() + 2 + ". Back");
+            System.out.print("> ");
+
+            buildingToLevelUp = Input.getInt(1, getVillage().getBuildings().size() + 2);
+
+            if (buildingToLevelUp == getVillage().getBuildings().size() + 2) {
+                playerInput();
+            } else {
+                try {
+                    if (buildingToLevelUp == 1) {
+                        getVillage().levelUp();
+                    } else {
+                        getVillage().levelUpBuilding(buildingToLevelUp - 2);
+                    }
+                } catch (InsufficientResourceException | MaxLevelException e) {
+                    System.out.println("\n\n" + e.getMessage() + "\n\n");
+                }
+            }
+
+        } else if (choice == 3) {
+            System.out.println("Choose troop to train\n ");
+
+            for (int i = 0; i < getVillage().getBuildings().stream().filter(building -> building instanceof TroopGenerator).count(); i++) {
+                TroopGenerator b = (TroopGenerator) getVillage().getBuildings().stream().filter(building -> building instanceof TroopGenerator).toArray()[i];
+                System.out.println((i + 1) + ". " + b.getInteractCostString() + " – " + b);
+            }
+
+            System.out.println(getVillage().getBuildings().stream().filter(building -> building instanceof TroopGenerator).count() + 1 + ". Back");
+
+            System.out.print("> ");
+
+            ArrayList<Integer> troopGenerators = new ArrayList<>();
+
+            for (Building building : getVillage().getBuildings()) {
+                if (building instanceof TroopGenerator) {
+                    troopGenerators.add(getVillage().getBuildings().indexOf(building));
+                }
+            }
+
+            int troopToTrain = Input.getInt(1, troopGenerators.size() + 1);
+
+            if (troopToTrain == troopGenerators.size() + 1) {
+                playerInput();
+                return;
+            } else {
+                try {
+                    getVillage().getBuildings().get(troopGenerators.get(troopToTrain - 1)).interact();
+                } catch (InsufficientResourceException | ArmyFullException | ArmyAwayException e) {
+                    System.out.println("\n\n" + e.getMessage() + "\n\n");
+                }
+            }
+
+        } else if (choice == 4) {
+
+            Input.clearScreen();
+            System.out.println("Choose a village to attack\n ");
+
+            ArrayList<Integer> villages = new ArrayList<>();
+
+            for (int i = 0; i < Map.players.size(); i++) {
+                if (!this.equals(Map.players.get(i))) {
+                    Player p = Map.players.get(i);
+                    System.out.println("Player " + p.getId() + "'s Village\n");
+                    System.out.println(Map.players.get(i).getVillage());
+                    System.out.println("\n");
+                    villages.add(i);
+                }
+            }
+
+            System.out.println(Map.players.size() + 1 + ". Back");
+            System.out.print("> ");
+
+
+            int villageToAttack = Input.getInt(1, Map.players.size() + 1);
+
+            if (villageToAttack == Map.players.size() + 1) {
+                playerInput();
+                return;
+            }
+
+
+            try {
+                getVillage().getArmy().initiateAttack(Map.players.get(villages.get(villageToAttack - 1)).getVillage());
+            } catch (ArmyEmptyException | ArmyBusyException e) {
+                System.out.println("\n\n" + e.getMessage() + "\n\n");
+            }
+
+
+        } else if (choice == 5) {
+            getVillage().setHealth(0);
+            return;
+        } else if (choice == 6) {
+            return;
+        }
+
+        playerInput();
     }
 }
