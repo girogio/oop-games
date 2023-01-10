a3064523069f6b18e4ab8ae19380792216aea982
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-01-10 17:37:55 +0100

    Fixed whitespace typo

--- task1/src/main/java/mt/grigolo/players/types/AI.java
+++ task1/src/main/java/mt/grigolo/players/types/AI.java
@@ -4,7 +4,7 @@ import mt.grigolo.players.Player;
 
 public class AI extends Player {
 
-    public AI(int x, int y, int id) {
+    public AI(int x, int y, int id){
         super(x, y, id);
     }
 
