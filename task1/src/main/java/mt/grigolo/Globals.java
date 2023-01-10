069282f32d3a7d3af872484ad6e140af00c0c8b5
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-01-10 18:46:29 +0100

    Formatted globals

--- task1/src/main/java/mt/grigolo/Globals.java
+++ task1/src/main/java/mt/grigolo/Globals.java
@@ -2,8 +2,6 @@ package mt.grigolo;
 
 public class Globals {
 
-    public static final float TroopGeneratorDiscountIncrease = 0.03F;
-    public static final int initialVillageResourceAmount = 1000;
 
     // Resource Generator
     public static final int resGenMaxLevel = 10;
@@ -18,29 +16,33 @@ public class Globals {
     public static final int initialVillageHealth = 1000;
     public static final int initialMaxTroops = 2;
     public static final int initialMaxResourceAmount = 1000;
+    public static final int initialVillageResourceAmount = 100;
     public static final int initialHealPerTick = 20;
 
+    // Troops
 
-    // Archer
-    public static final int archerCost = 10;
-    public static final int archerDamage = 10;
-    public static final int archerSpeed = 2;
-    public static final int archerHealth = 100;
-    public static final int archerMaxInventory = 10;
+    public static final float TroopGeneratorDiscountIncrease = 0.03F;
 
-    // Barbarian
-    public static final int barbarianCost = 10;
-    public static final int barbarianDamage = 10;
-    public static final int barbarianSpeed = 2;
-    public static final int barbarianHealth = 100;
-    public static final int barbarianMaxInventory = 10;
+        // Archer
+        public static final int archerCost = 10;
+        public static final int archerDamage = 10;
+        public static final int archerSpeed = 2;
+        public static final int archerHealth = 100;
+        public static final int archerMaxInventory = 10;
 
-    // Goblin
-    public static final int goblinCost = 10;
-    public static final int goblinDamage = 10;
-    public static final int goblinSpeed = 2;
-    public static final int goblinHealth = 100;
-    public static final int goblinMaxInventory = 30;
+        // Barbarian
+        public static final int barbarianCost = 10;
+        public static final int barbarianDamage = 10;
+        public static final int barbarianSpeed = 2;
+        public static final int barbarianHealth = 100;
+        public static final int barbarianMaxInventory = 10;
+
+        // Goblin
+        public static final int goblinCost = 10;
+        public static final int goblinDamage = 10;
+        public static final int goblinSpeed = 2;
+        public static final int goblinHealth = 100;
+        public static final int goblinMaxInventory = 30;
 
 
 }
