41db94c08055f66d23dae6365ebb8db6b9307dd2
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-01-10 18:46:57 +0100

    Fixed main class

--- task1/src/main/java/mt/grigolo/Main.java
+++ task1/src/main/java/mt/grigolo/Main.java
@@ -5,39 +5,38 @@ import mt.grigolo.game.Game;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
 
-//        int playerCount;
-//        int aiCount;
-//        int w;
-//        int h;
+       int playerCount;
+       int aiCount;
+       int w;
+       int h;
 
-//        do {
-//            System.out.print("Enter number of players (1 or more): ");
-//            playerCount = s.nextInt();
-//        } while (playerCount < 1);
-//
-//
-//        do {
-//            System.out.print("Enter number of AIs (0 or more): ");
-//            aiCount = s.nextInt();
-//        } while (aiCount < 0);
-//
-//        do {
-//            System.out.print("Enter map width: ");
-//            w = s.nextInt();
-//        } while (w < 0);
-//
-//        do {
-//            System.out.print("Enter map height: ");
-//            h = s.nextInt();
-//        } while (h < 0);
+       do {
+           System.out.print("Enter number of players (1 or more): ");
+           playerCount = s.nextInt();
+       } while (playerCount < 1);
 
-//        Game game = new Game(playerCount, aiCount, w, h);
 
-        Game game = new Game(1, 1, 10, 10);
+       do {
+           System.out.print("Enter number of AIs (0 or more): ");
+           aiCount = s.nextInt();
+       } while (aiCount < 0);
+
+       do {
+           System.out.print("Enter map width: ");
+           w = s.nextInt();
+       } while (w < 0);
+
+       do {
+           System.out.print("Enter map height: ");
+           h = s.nextInt();
+       } while (h < 0);
+
+       Game game = new Game(playerCount, aiCount, w, h);
 
         game.startGame();
 
