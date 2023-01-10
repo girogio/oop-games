a4562118c79a3bd69586d6b2730cc3c6d1cd6531
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-01-10 17:25:33 +0100

    Code reformat, and fixed cloning of troops

--- task1/src/main/java/mt/grigolo/buildings/types/TroopGenerator.java
+++ task1/src/main/java/mt/grigolo/buildings/types/TroopGenerator.java
@@ -15,27 +15,33 @@ public abstract class TroopGenerator extends Building {
 
     private final Troop troopGenerated;
 
+    public abstract Troop generatedTroop();
+
     private final Resource resourceConsumed;
 
     private float discount = 0;
 
-    private final int baseCost;
+    public float getDiscount() {
+        return discount;
+    }
+
+    private final int interactCost;
+
+    public String getInteractCostString() {
+        return interactCost + " " + getLevelUpResource().getName();
+    }
 
     public TroopGenerator(Troop troopGenerated, Army targetArmy, Resource resourceConsumed) {
         super(resourceConsumed, 10, 30, 50, 50);
         this.troopGenerated = troopGenerated;
         this.targetArmy = targetArmy;
-        this.baseCost = troopGenerated.getCost();
+        this.interactCost = troopGenerated.getCost();
         this.resourceConsumed = resourceConsumed;
     }
 
     @Override
     public void levelUpLogic() {
         discount += Globals.TroopGeneratorDiscountIncrease;
-    }
-
-    public float getDiscount() {
-        return discount;
     }
 
     @Override
@@ -46,7 +52,7 @@ public abstract class TroopGenerator extends Building {
     @Override
     public void interact() throws InsufficientResourceException, ArmyFullException, ArmyAwayException {
         if (targetArmy.isInHomeVillage()) {
-            int cost = (int) (baseCost * (1 - discount));
+            int cost = (int) (interactCost * (1 - discount));
             if (resourceConsumed.getAmount() < cost) {
                 throw new InsufficientResourceException(cost - resourceConsumed.getAmount());
             } else {
@@ -58,8 +64,6 @@ public abstract class TroopGenerator extends Building {
             throw new ArmyAwayException();
         }
     }
-
-    abstract Troop generatedTroop();
 
     @Override
     public String toString() {
